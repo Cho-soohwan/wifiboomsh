12355142e255cf9bab6e1db706468edaf60e13fa
Cho-soohwan <58143134+Cho-soohwan@users.noreply.github.com>
2020-12-10 15:46:18 +0900

    Add files via upload

--- wifiboomsh.py
+++ wifiboomsh.py
@@ -16,8 +16,8 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 #############
 os.system("clear")
-os.system("figlet DDos Attack")
-print "CHOSOOHWAN WIFI BOOM!"
+os.system("figlet WIFIBOOM")
+print "CHOSOOHWAN"
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 os.system("clear")
